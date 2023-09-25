9c63823e4e9c3b3661d9496e2811b56645f04f4c
David <dvdslab@gmail.com>
2023-09-25 23:19:21 +0100

    done in call

--- src/coin_gift_app_frontend/pages/LandingPage.tsx
+++ src/coin_gift_app_frontend/pages/LandingPage.tsx
@@ -1,12 +1,44 @@
-import React from "react";
-import { Box, Text, Image } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Box, Text, Image, Center, Flex, Button } from "@chakra-ui/react";
 import { coin_gift_app_backend } from "../../declarations/coin_gift_app_backend/index";
+import { userInput } from "nexai-assistant";
+import { useInitTransformers, useEmbeddQ } from "../ml";
+import { nexai } from "../../declarations/nexai";
+
 
 function LandingPage() {
+
+  const { init } = useInitTransformers()
+  const { call, embeddedQ } = useEmbeddQ()
+
+
+  const clickMe = async () => {
+    await call("text");
+    if (embeddedQ[0].length == 384) {
+      nexai.VDBGetSimilar(1, embeddedQ[0], 1).then((d) => {
+        console.log(d)
+      }).catch((err) => {
+        console.log(err);
+      })
+
+    }
+  }
+
+  useEffect(() => {
+    const call = async () => {
+      await init()
+    }
+    call()
+  }, [])
   return (
     <Box>
-      <Text>Coin Gift App</Text>
-      <Image src={`coin-1.jpg`} />
+      <Flex>
+        <Text fontSize={'100px'} pb={'50px'}>Coin Gift App {userInput}</Text>
+      </Flex>
+      <Center>
+        <Button onClick={clickMe}>test integration</Button>
+      </Center>
+      {/* <Text fontSize={'10px'}>Coin Gift App</Text> */}
     </Box>
   );
 }
